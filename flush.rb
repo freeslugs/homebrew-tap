d49f038ffe59ec74de299dacb304a10f3e5e8be8
Gilad Penn <freeslugs@gmail.com>
2020-07-17 11:47:55 -0400

    correct sha

--- flush.rb
+++ flush.rb
@@ -2,7 +2,7 @@ class Flush < Formula
   homepage "://github.com/freeslugs/flush"
   url "https://github.com/freeslugs/flush/archive/0.0.2.tar.gz"
   version "0.0.2"
-  sha256 "486b79bedb4b8095a0c955490ef39b5770e14569892eda6513280e0d2287b4dc"
+  sha256 "cf172669103412f93aa78a6d9292f067fe0672e355a8aab341c347a0d9f51a22"
 
   # shasum -a 256 v1.1.tar.gz
 
