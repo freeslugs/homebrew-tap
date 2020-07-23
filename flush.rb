ce3d062811a7f738d4df6749e17ecb2ad635cbba
Gilad Penn <freeslugs@gmail.com>
2020-07-23 12:52:17 -0400

    release 0.0.7

--- flush.rb
+++ flush.rb
@@ -1,8 +1,8 @@
 class Flush < Formula
   homepage "://github.com/freeslugs/flush"
-  url "https://github.com/freeslugs/flush/archive/0.0.6.tar.gz"
-  version "0.0.6"
-  sha256 "550fbe5079b5ea452b8c508accecaa8e27490f6a48bb787a73781415530b8789"
+  url "https://github.com/freeslugs/flush/archive/0.0.7.tar.gz"
+  version "0.0.7"
+  sha256 "c26db1888daadbf64783fb666d1c53f6ebe61c7dc96fa031054efe19254c9bf2"
 
   # shasum -a 256 v1.1.tar.gz
 
